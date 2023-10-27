019e3484d58d033c5e937cffb84298143497260b
YukiP7 <gargyukti112@gmail.com>
2023-10-27 13:16:49 +0530

    Apply changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import MovieCard from './components/MovieCard';
-import axios from 'axios';
-import NavStyle from './components/Navbar.jsx';
+import Card from './components/MovieCard';
+import Nav from './components/Navbar.jsx';
 import './app.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCircleChevronLeft ,faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
+import Footer from './components/footer';
 
-function scrollLeft(id) {
-  const c = document.querySelector(".box");
-  const scrollAmount = -c.firstChild.clientWidth; // Scroll one container width to the left
-  c.style.transform = `translateX(${scrollAmount}px)`;
-}
 
-function scrollRight(id) {
-  const c = document.querySelector(".box");
-  const scrollAmount = c.lastChild.clientWidth; // Scroll one container width to the right
-  c.style.transform = `translateX(${scrollAmount}px)`;
-}
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const apiKey = 'd59f0303'; 
-    const apiUrl = `http://www.omdbapi.com/?s=movie&type=movie&apikey=${apiKey}`;
-
-    axios
-      .get(apiUrl)
-      .then((response) => {
-        setMovies(response.data.Search);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching movie data from OMDB:', error);
-        setLoading(false);
-      });
-  }, []);
-
   
   return (
     <div>
-      <NavStyle/>
+      <Nav/>
       <div className="heading">
         <p id="heading">Popular Movies</p>
         <hr/>
       </div>
-        <div className="container">
-       
-        <button id="scroll-left" onClick={scrollLeft}> <FontAwesomeIcon icon={faCircleChevronLeft} /></button>
-        
-        <div className="box">
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          movies.map((movie) => (
-            <MovieCard
-              key={movie.imdbID}
-              title={movie.Title}
-              imageUrl={movie.Poster}
-              description={movie.Year}
-            />
-          ))
-        )}
-        </div>
-
-        <button id="scroll-right" onClick={scrollRight}> <FontAwesomeIcon icon={faCircleChevronRight} /></button>
-      </div>
+            <Card/>
       
       <div className="heading">
         <p id="heading">Upcoming Movies</p>
         <hr/>
       </div>
-        <div className="container">
-        <button id="scroll-left" onClick={scrollLeft}> <FontAwesomeIcon icon={faCircleChevronLeft} /></button>
-        
-        <div className="box">
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          movies.map((movie) => (
-            <MovieCard
-              key={movie.imdbID}
-              title={movie.Title}
-              imageUrl={movie.Poster}
-              description={movie.Year}
-            />
-          ))
-        )}
-        </div>
-        <button id="scroll-right" onClick={scrollRight}> <FontAwesomeIcon icon={faCircleChevronRight} /></button>
-        </div>
+            <Card/>
+
+            <Footer/>
       </div>
+
+
   );
 };
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import  { useState , useEffect } from "react"
+import './movie.css'
 
-const MovieCard = ({ title, imageUrl, description }) => {
+
+const apiKey = "dfccb9b3ae7dbbd11879b0f571cfe1ea" ;
+const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&page=1&with_genres=28` ;
+
+function Card(){
+    const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.results);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="max-w-xs w-half rounded overflow-hidden shadow-lg m-2">
-      <img className="w-full " src={imageUrl} alt={title} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-l mb-2">{title}</div>
-        <p className="text-gray-600 text-base">{description}</p>
-      </div>
+    <div className=" rounded overflow-hidden shadow-lg m-2">
+      <div className="scroll-container">
+        {movies.map((movie, index) => (
+          <div key={index} className="image-wrapper">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              alt={movie.title}
+              className="movie-image"
+            />
+          </div>
+        ))}
+        </div>
     </div>
   );
 };
 
-export default MovieCard;
+export default Card;
